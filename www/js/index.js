721db9b1ca98aef64fc8a79bc31cf33e848d8254
blancamalerva <blanca.malerva@gmail.com>
2020-07-10 12:11:53 -0500

    add if

--- www/js/index.js
+++ www/js/index.js
@@ -27,6 +27,19 @@ var app = {
     // Bind any cordova events here. Common events are:
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
+      function checkConnection() {
+        var networkState = navigator.network.connection.type;
+        var states = {};
+        states[Connection.UNKNOWN] = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI] = 'WiFi connection';
+        states[Connection.CELL_2G] = 'Cell 2G connection';
+        states[Connection.CELL_3G] = 'Cell 3G connection';
+        states[Connection.CELL_4G] = 'Cell 4G connection';
+        states[Connection.NONE] = 'No network connection';
+        return networkState;
+      }  
+      
       var networkState = checkConnection();
       /* load local files if there is not network connection */
       if (networkState == Connection.NONE) {
