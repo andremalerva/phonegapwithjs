35f587bf5ba48751032fce489a3309740f22673c
blancamalerva <blanca.malerva@gmail.com>
2020-07-10 14:18:13 -0500

    mod line offline line console

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -47,11 +47,11 @@ module.exports.metadata =
 // TOP OF METADATA
 {
   "cordova-plugin-actionsheet": "2.3.3",
+  "cordova-plugin-console": "1.1.0",
   "cordova-plugin-inappbrowser": "1.7.1",
   "cordova-plugin-network-information": "1.3.3",
   "cordova-plugin-spinner-dialog": "1.3.1",
-  "cordova-plugin-whitelist": "1.3.2",
-  "cordova-plugin-console": "1.1.0"
+  "cordova-plugin-whitelist": "1.3.2"
 };
 // BOTTOM OF METADATA
 });
